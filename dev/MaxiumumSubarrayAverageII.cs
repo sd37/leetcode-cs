5f3d16c6893bfd5ce9a481cd71621325dd70c067
spandan dutta <spandan_dutta@alumni.brown.edu>
2020-11-29 17:57:04 -0800

    sln

--- dev/MaxiumumSubarrayAverageII.cs
+++ dev/MaxiumumSubarrayAverageII.cs
@@ -1,5 +1,18 @@
 // https://leetcode.com/problems/maximum-average-subarray-ii/solution/
 // Status = TLE (59/74) pass
+// You are given an integer array nums consisting of n elements, and an integer k.
+//
+// Find a contiguous subarray whose length is greater than or equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+// Input: nums = [1,12,-5,-6,50,3], k = 4
+// Output: 12.75000
+// Explanation:
+// - When the length is 4, averages are [0.5, 12.75, 10.5] and the maximum average is 12.75
+// - When the length is 5, averages are [10.4, 10.8] and the maximum average is 10.8
+// - When the length is 6, averages are [9.16667] and the maximum average is 9.16667
+// The maximum average is when we choose a subarray of length 4 (i.e., the sub array [12, -5, -6, 50]) which has the max average 12.75, so we return 12.75
+// Note that we do not consider the subarrays of length < 4.
+
+
 namespace dev
 {
     using System;
